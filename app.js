5e7401d3291b2b0ec637ad5f7aecc2b88199e430
ujasitalia <ujasitalia216@gmail.com>
2023-04-27 18:20:38 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', async (req, res) => {
+    res.json({
+        mesg: "Hello from Backend"
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log("Your Server has started on http://localhost:3000");
+})
